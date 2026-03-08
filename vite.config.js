172166a578ecdd962c6740b7a9d7f737b9c888fc
Jimmy Taylor <11437306+consentfactory@users.noreply.github.com>
2026-03-07 17:44:38 -0700

    Fix lineup page

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
   root: 'src',
@@ -11,5 +12,12 @@ export default defineConfig({
   build: {
     outDir: '../dist',
     emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'src/index.html'),
+        lineup: resolve(__dirname, 'src/lineup.html'),
+        setup: resolve(__dirname, 'src/setup.html'),
+      },
+    },
   },
 });
